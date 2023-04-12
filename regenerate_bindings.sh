27819f4cf1f98d037af18b7c6a22bc3f20836813
Reinier Balt <lrbalt@gmail.com>
2023-04-12 15:17:44 +0200

    regenerate bindings

--- regenerate_bindings.sh
+++ regenerate_bindings.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-bindgen --size_t-is-usize --no-prepend-enum-name /usr/local/include/soxr.h -o src/generated.rs
+bindgen --size_t-is-usize --no-prepend-enum-name /usr/include/soxr.h -o src/generated.rs
