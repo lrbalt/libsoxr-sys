57d876673c059636e881e505e2258e8e724ff68c
Giang Minh <giangndm@ows.vn>
2023-12-13 21:41:21 +0700

    fix: build failed on github-actions with macos-aarch64

--- build.rs
+++ build.rs
@@ -1,19 +1,52 @@
+use std::{path::Path, fmt::Display};
+
 extern crate pkg_config;
 
 fn main() {
     if std::env::var("DOCS_RS").is_err() {
         // do not probe for libsoxr when compiling at docs.rs
-        if let Err(e) = pkg_config::probe_library("soxr") {
-            match e {
-            pkg_config::Error::Failure { .. } => panic! (
-                "Pkg-config failed - usually this is because libsoxr development headers are not installed.\n\n\
-                For Mac users using brew: brew install libsoxr\n\n\
-                For Debian/Ubuntu users:\n# apt-get install libsoxr0-dev\n\n\
-                pkg_config details:\n{}",
-                e
-            ),
-            _ => panic!("{}", e)
-        }
+        if let Err(_e) = pkg_config::probe_library("soxr") {
+            build_soxr(true);
         }
     }
+}
+
+/// Outputs the library-file's prefix as word usable for actual arguments on
+/// commands or paths.
+const fn rustc_linking_word(is_static_link: bool) -> &'static str {
+    if is_static_link {
+        "static"
+    } else {
+        "dylib"
+    }
+}
+
+fn build_soxr(is_static: bool) {
+    let soxr_path = Path::new("soxr");
+
+    println!(
+        "cargo:info=Soxr source path used: {:?}.",
+        soxr_path
+            .canonicalize()
+            .expect("Could not canonicalise to absolute path")
+    );
+
+    println!("cargo:info=Building Soxr via CMake.");
+    let soxr_build_dir = cmake::Config::new("soxr")
+                .profile("Release")
+                .define("WITH_CR32S", "OFF")
+                .define("BUILD_SHARED_LIBS", "OFF")
+                .build();
+    link_soxr(is_static, soxr_build_dir.display())
+}
+
+fn link_soxr(is_static: bool, soxr_build_dir: impl Display) {
+    let is_static_text = rustc_linking_word(is_static);
+
+    println!(
+        "cargo:info=Linking Soxr as {} lib: {}",
+        is_static_text, soxr_build_dir
+    );
+    println!("cargo:rustc-link-lib={}=soxr", is_static_text);
+    println!("cargo:rustc-link-search=native={}/lib", soxr_build_dir);
 }
